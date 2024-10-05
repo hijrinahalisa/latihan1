9539e677384e724e68c07ba509fe933e6843f117
hijrinahalisa <hijrinahalisa2003@gmail.com>
2024-10-04 23:41:12 -0700

    done

---
+++ bin/dart.dasar/For_In.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  
+
+  
+  var names = <String>['Hijrina', 'Halisa',];
+
+  for (var value in names) {
+    print(value);
+  }
+
+  //Gunakan set
+  var nameSet = <String>{'Hijrina', 'Halisa',};
+  for (var value in nameSet) {
+    print(value);
+  }
+}
